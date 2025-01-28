0fd6d6425c83b1202e10ecc4339aff9390862566
nabyungho <covaloon2@codeindesign.kr>
2025-01-29 01:58:33 +0900

    [feat] 한입북스 UI 구현 중

---
+++ src/components/book/book-item.tsx
@@ -0,0 +1,26 @@
+import {BookData} from "@/types/bookItem/book.type";
+import Link from "next/link";
+import style from "./book-item.module.css";
+
+export default function BookItem(
+    {
+        id,
+        title,
+        subTitle,
+        description,
+        author,
+        publisher,
+        coverImgUrl }: BookData)
+{
+    return (<Link href={`/book/${id}`} className={style.container}>
+        <img src={coverImgUrl}/>
+        <div>
+            <div className={style.title}>{title}</div>
+            <div className={style.title}>{subTitle}</div>
+            <br/>
+            <div>
+                {author} | {publisher}
+            </div>
+        </div>
+    </Link>);
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,24 @@
 import {ReactNode} from "react";
 import SearchableLayout from "@/components/layout/searchable-layout/searchable-layout";
+import style from "../index.module.css";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book/book-item";
 
 export default function Home() {
     return (
-        <div>
-        Home
+        <div className={style.container}>
+            <section>
+                <h3>지금 추천하는 도서</h3>
+                {books.map(
+                    (book) => (<BookItem key={book.id} {...book}/>)
+                )}
+            </section>
+            <section>
+                <h3>등록된 모든 도서</h3>
+                {books.map(
+                    (book) => (<BookItem key={book.id} {...book}/>)
+                )}
+            </section>
         </div>
     );
 }
