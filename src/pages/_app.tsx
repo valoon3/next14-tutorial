1d1dffdecf85d036a35d605cbc3a936a911c2167
nabyungho <covaloon2@codeindesign.kr>
2025-01-28 18:00:35 +0900

    [feat] prefetching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,34 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  function onClickButton() {
+    router.push("/test");
+  }
+
+  useEffect(() => {
+    router.prefetch("/test");
+  }, [])
+
   return (<>
-    <h1>global header</h1>
+    <header>
+      <Link href={"/"}>index</Link>
+      &nbsp;
+      <Link href={"/search/book/1"}>book</Link>
+      &nbsp;
+      <Link href={"/search"} prefetch={false}>
+        search
+      </Link>
+      &nbsp;
+      <div>
+        <button onClick={onClickButton}>test</button>
+      </div>
+    </header>
     <Component {...pageProps} />
   </>);
 }
