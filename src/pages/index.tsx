cfc03d58c13c95abf57cd0be6f883b73ad935555
nabyungho <covaloon2@codeindesign.kr>
2025-01-28 21:42:48 +0900

    [feat] styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 
+import styles from "@/pages/index.module.css";
 
 export default function Home() {
   return (
     <>
-      <h1>hello world</h1>
+      <h1 className={ styles.h1 }>hello world</h1>
     </>
   );
 }
