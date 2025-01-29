25768bd6f2267a817cc1882501613f49a75f24b8
nabyungho <covaloon2@codeindesign.kr>
2025-01-29 16:57:05 +0900

    [feat] 한입북스 UI 구현 완료

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,4 +1,6 @@
 import {useRouter} from "next/router";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book/book-item";
 
 export default function Page() {
     const router = useRouter();
@@ -8,8 +10,10 @@ export default function Page() {
     console.log(router);
 
     return (
-        <>
-            <h1>Search {q}</h1>
-        </>
+        <div>
+            {books.map((book) => (
+                <BookItem key={book.id} {...book}/>
+            ))}
+        </div>
     );
 }
